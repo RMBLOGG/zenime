97cef2848072ff33523d66a6a2c57a20293ecb1c
RMBLOGG <ramdanhidayah176@gmail.com>
2026-09-11 08:49:53 +0700

    add CI workflow

--- app/src/main/java/com/example/ads/AdManager.kt
+++ app/src/main/java/com/example/ads/AdManager.kt
@@ -229,7 +229,7 @@ object AdManager {
                     onRewardedShowFinished(earned = pendingRewardedEarned)
                 }
 
-                override fun onAdRewarded(adInfo: LevelPlayAdInfo, reward: LevelPlayReward) {
+                override fun onAdRewarded(reward: LevelPlayReward, adInfo: LevelPlayAdInfo) {
                     // Iklan udah ditonton sampai selesai -- user berhak dapet
                     // reward. onAdClosed tetap akan kepanggil setelah ini,
                     // jadi kita simpan flag-nya dan biarkan onAdClosed yang
